e14a16fc39be54e2bf6506ba72027269708d891a
kenneth <gilmorekenneth@gmail.com>
2020-02-29 22:55:07 -0500

    Started working on mastermind game

---
+++ mastermind.rb
@@ -0,0 +1,22 @@
+colors = {
+  red: 1, 
+  green: 2,
+  blue: 3,
+  yellow: 4,
+  gold: 5,
+  orange: 6,
+  black: 7,
+  white: 8
+}
+
+# class colors
+# end
+
+
+helper_key = []
+secret_key = []
+
+puts colors[:green]
+2.times puts "#"*54
+puts "#"*12 + " "*4 + "Welcome To Mastermind!" + " "*4 + "#"*12
+2.times puts "#"*54
